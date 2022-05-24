5d9c351cf25df162c929452325667e8592e8548c
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-05-24 16:28:49 +0900

    220524_3_195_CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<int:pk>/', views.single_post_page),
-    path('/', views.index),
+    # path('/<int:pk>/', views.single_post_page), # this was FBV
+    # path('/', views.index), # this was FBV
+    path('/<int:pk>/', views.PostDetail.as_view()),
+    path('/', views.PostList.as_view()),
 ]
